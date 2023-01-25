38540ab5cbe12a0cd1e2148302aa766b712f53c9
Beau McCartney <beau@beaumccartney.xyz>
2023-01-25 11:13:52 -0700

    separate error message for failing to open file

--- error.c
+++ error.c
@@ -5,7 +5,7 @@
 
 void error(const char *msg, int linenum) {
     fprintf(stderr, "Error: %s\nLine number: %d\nExiting...\n", msg, linenum);
-    exit(1);
+    exit(EXIT_FAILURE);
 }
 
 void warning(const char *msg, int linenum) {
@@ -16,6 +16,6 @@ void warning(const char *msg, int linenum) {
     const static int max_warnings = 15;
     if (num_warnings >= max_warnings) {
         fprintf(stderr, "Max warnings reached (that's %d warnings!). Exiting...\n", max_warnings);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #include "error.h"
 
@@ -6,10 +9,16 @@ int main(int argc, char *argv[]) {
     FILE *fp;
 
     // NOTE: I don't use my error routine here because there's no line number to point to
-    if (argc != 2 || !( fp = fopen(argv[1], "r") )) {
+    if ( argc != 2 ) {
         fprintf(stderr, "Usage: %s <filename>\n\n", argv[0]);
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+
+    if ( !(fp = fopen(argv[1], "r")) ) {
+        fprintf(stderr, "Could not open file '%s': %s\n", argv[1], strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
