a09bb169c7484676a46e326cebe22b008dcea5c8
Beau McCartney <beau@beaumccartney.xyz>
2023-01-25 16:56:13 -0700

    is_space(): spec-conformant whitespace test

--- lex.cpp
+++ lex.cpp
@@ -1,6 +1,7 @@
 #include "lex.hpp"
 #include "error.hpp"
 
+bool is_space(int c) { return c == '\n' || c == '\t' || c == ' ' || c == '\r'; }
 
 Tokinfo lex(FILE *fp) {
     // TODO: other attribute information (col number etc)?
@@ -8,7 +9,7 @@ Tokinfo lex(FILE *fp) {
     Tokinfo result;
 
     // spin until we find something interesting
-    while ( ( ch = getc(fp) ) != EOF && isspace(ch) )
+    while ( ( ch = getc(fp) ) != EOF && is_space(ch) )
         if (ch == '\n') linenum++;
 
     switch (ch) {
