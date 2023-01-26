0b8d28515873444cbe7fbdffbd5ebb70f083fb32
Beau McCartney <beau@beaumccartney.xyz>
2023-01-25 20:19:43 -0700

    reference_output.sh and its output
    the script simply runs the reference compiler on each test case in order
    and wraps the output with some info to make parsing easier
    
    useful for piping into a pager or redirecting to a file (the latter of
    which I've also done)

---
+++ reference_output.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo "REFERENCE OUTPUT"
+echo "================"
+
+# NOTE: ik using globs is nicer but I want the test outputs in order so I do it this way
+for testnum in $(seq 1 30)
+do
+    filename="scan.t$testnum"
+    printf "\n\nBEGIN OUTPUT FOR %s\n" "$filename"
+    echo "================"
+    # NOTE: `golf_reference` is a symlink pointing to the reference compiler,
+    #       and `ms1` is a symlink pointing to the ms1 directory with all the tests
+    ./golf_reference "ms1/$filename"
+    echo "================"
+    printf "END OUTPUT FOR %s\n\n" "$filename"
+done
