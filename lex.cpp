9d53362c9738fda710cf079bd6e0bfb997983c73
Beau McCartney <beau@beaumccartney.xyz>
2023-01-25 18:51:58 -0700

    lex(): init result and put members in localscope

--- lex.cpp
+++ lex.cpp
@@ -6,13 +6,19 @@ bool is_space(int c) { return c == '\n' || c == '\t' || c == ' ' || c == '\r'; }
 bool is_letter(int c) { return isalpha(c) || c == '_'; }
 
 Tokinfo lex(FILE *fp) {
-    // TODO: other attribute information (col number etc)?
-    int ch, linenum = 1;
+    // init result and put its members in local scope
     Tokinfo result;
+    auto & linenum = result.linenum;
+    auto & token   = result.token;
+    auto & lexeme  = result.lexeme;
 
+
+    int ch;
     // spin until we find something interesting
     while ( ( ch = getc(fp) ) != EOF && is_space(ch) )
         if (ch == '\n') linenum++;
+
+    lexeme = ch; // first (and possibly only) character of the lexeme
 
     switch (ch) {
         // NULL character isn't allowed in input
