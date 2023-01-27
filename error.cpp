e6f980f8e75c152c0a2990cb5e809221df4cd0da
Beau McCartney <beau@beaumccartney.xyz>
2023-01-27 16:24:27 -0700

    warning and error messages for bad chars

--- error.cpp
+++ error.cpp
@@ -3,24 +3,37 @@
 
 #include "error.hpp"
 
-// TODO: routines that don't take line numbers?
-// TODO: routines that take format strings + args and print them
-// ^^ above is to allow me to print what character I'm skipping
-
 void error(const char *msg, int linenum) {
     fprintf(stderr, "Error: %s at line number: %d\n", msg, linenum);
     exit(EXIT_FAILURE);
 }
 
-void warning(const char *msg, int linenum) {
-    fprintf(stderr, "Warning: %s at line number: %d\n", msg, linenum);
+void bad_char_error(const char *msg, char badchar, int linenum) {
+    fprintf(stderr, "Error: %s '%c' at line number: %d\n", msg, badchar, linenum);
+    exit(EXIT_FAILURE);
+}
 
-    // fail on warning cascade
-    static int num_warnings = 0;
-    num_warnings++;
-    const static int max_warnings = 15;
+// track number of warnings given
+static int num_warnings = 0;
+const static int max_warnings = 15;
+
+void handle_warning_cascade() {
     if (num_warnings >= max_warnings) {
         fprintf(stderr, "Max warnings reached (that's %d warnings!). Exiting...\n", max_warnings);
         exit(EXIT_FAILURE);
     }
+}
+
+void warning(const char *msg, int linenum) {
+    fprintf(stderr, "Warning: %s at line number: %d\n", msg, linenum);
+
+    num_warnings++;
+    handle_warning_cascade();
+}
+
+void bad_char_warning(const char *msg, char badchar, int linenum) {
+    fprintf(stderr, "Warning: %s '%c' at line number: %d\n", msg, badchar, linenum);
+
+    num_warnings++;
+    handle_warning_cascade();
 }
--- error.hpp
+++ error.hpp
@@ -7,3 +7,9 @@ void error(const char *msg, int linenum);
 
 // warning message for recoverable errors
 void warning(const char *msg, int linenum);
+
+// warning message for giving information about bad characters
+void bad_char_error(const char *msg, char badchar, int linenum);
+
+// warning message for giving information about bad characters
+void bad_char_warning(const char *msg, char badchar, int linenum);
