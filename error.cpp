47657faf89f9060a93b72eace31715c2e3425276
Beau McCartney <beau@beaumccartney.xyz>
2023-01-27 21:11:08 -0700

    use applicable exit codes from sysexits.h

--- error.cpp
+++ error.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <sysexits.h>
 
 #include "error.hpp"
 
@@ -27,13 +28,13 @@ const std::string ascii_char_to_string( int c ) {
 
 void error( const char *msg, int linenum ) {
     fprintf( stderr, "error: %s at or near line %d\n", msg, linenum );
-    exit( EXIT_FAILURE );
+    exit( EX_DATAERR );
 }
 
 void bad_char_error( const char *msg, char badchar, int linenum ) {
     const std::string badchar_str = ascii_char_to_string( badchar );
     fprintf( stderr, "error: %s '%s' at or near line %d\n", msg, badchar_str.data(), linenum );
-    exit( EXIT_FAILURE );
+    exit( EX_DATAERR );
 }
 
 // track number of warnings given
@@ -43,7 +44,7 @@ const static int max_warnings = 10;
 void handle_warning_cascade( int linenum ) {
     if ( num_warnings >= max_warnings ) {
         error( "too many warnings", linenum );
-        exit( EXIT_FAILURE );
+        exit( EX_DATAERR );
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <sysexits.h>
 
 #include "error.hpp"
 #include "lex.hpp"
@@ -11,7 +12,7 @@ int main( int argc, char *argv[] ) {
     // NOTE: I don't use my error routine here because the error message's format is much different
     if ( argc != 2 ) {
         fprintf( stderr, "Usage: %s <filename>\n\n", argv[0] );
-        return EXIT_FAILURE;
+        return EX_USAGE;
     }
 
 
@@ -19,12 +20,12 @@ int main( int argc, char *argv[] ) {
     // fail if file can't be opened
     if ( !( fp = fopen( argv[1], "r" ) ) ) {
         fprintf( stderr, "Could not open file '%s': %s\n", argv[1], strerror( errno ) );
-        return EXIT_FAILURE;
+        return EX_NOINPUT;
     }
 
     // print each token except EOF
     for ( Tokinfo t = lex( fp ); t.token != TOKEN_EOF; t = lex( fp ) )
         printf( "%s\t[%s] @ line %d\n", token_to_string( t.token ), t.lexeme.data(), t.linenum );
 
-    return EXIT_SUCCESS;
+    return EX_OK;
 }
