d7eb722a4c5fe6d074068eee1fbb07f63851da63
Beau McCartney <beau@beaumccartney.xyz>
2023-02-24 02:06:55 -0700

    only print line number if its known

--- parse.cpp
+++ parse.cpp
@@ -547,30 +547,30 @@ ASTNode parse() {
 
 // it's 11 at night and i've been coding since 7 am. forgive me
 std::string ASTNode_to_string( ASTNode n ) {
-    std::string numstring = std::to_string( n.linenum );
+    std::string numstring;
+    if ( n.linenum > 0 ) numstring = "@ line " + std::to_string( n.linenum );
 
     std::string lexstring;
-    if ( n.lexeme.size() > 0)
-        lexstring = "[" + n.lexeme + "]";
+    if ( n.lexeme.size() > 0) lexstring = "[" + n.lexeme + "] ";
 
     switch ( n.type ) {
         case AST_PROGRAM:   return "program";
-        case AST_FUNC:      return "func @ line " + numstring;
-        case AST_NEWID:     return "newid " + lexstring + " @ line " + numstring;
+        case AST_FUNC:      return "func " + numstring;
+        case AST_NEWID:     return "newid " + lexstring + numstring;
         case AST_SIGNATURE: return "sig";
         case AST_FORMALS:   return "formals";
-        case AST_TYPEID:    return "typeid " + lexstring + " @ line " + numstring;
+        case AST_TYPEID:    return "typeid " + lexstring + numstring;
         case AST_BLOCK:     return "block";
-        case AST_FOR:       return "for @ line " + numstring;
-        case AST_ID:        return "id " + lexstring + " @ line " + numstring;
+        case AST_FOR:       return "for " + numstring;
+        case AST_ID:        return "id " + lexstring + numstring;
         case AST_EMPTYSTMT: return "emptystmt";
-        case AST_IF:        return "if @ line " + numstring;
-        case AST_EQ:        return "== @ line "  + numstring;
-        case AST_PLUS:      return "+ @ line "   + numstring;
-        case AST_INT:       return "int " + lexstring + " @ line " + numstring;
+        case AST_IF:        return "if " + numstring;
+        case AST_EQ:        return "== "  + numstring;
+        case AST_PLUS:      return "+ "   + numstring;
+        case AST_INT:       return "int " + lexstring + numstring;
         case AST_EXPR:      return "expr";
-        case AST_ASSIGN:    return "= @ line " + numstring;
-        case AST_STAR:      return "* @ line " + numstring;
+        case AST_ASSIGN:    return "= " + numstring;
+        case AST_STAR:      return "* " + numstring;
         default:
             printf( "thing is: %d '%s'\n", n.type, n.lexeme.data() );
             return "foo";
