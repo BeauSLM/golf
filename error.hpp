04d4535a58d3324e61fc2b573a9bbfbfc167d4e2
Beau McCartney <beau@beaumccartney.xyz>
2023-03-11 20:27:34 -0700

    remove unneeded function definitions
    these are from before I wrapped printf, how that I'm wrapping printf
    they don't matter at all

--- error.hpp
+++ error.hpp
@@ -10,11 +10,5 @@ void error( int linenum, const char *msg, ... );
 // warning message for recoverable errors
 void warning( int linenum, const char *msg, ... );
 
-// warning message for giving information about bad characters
-void bad_char_error( const char *msg, char badchar, int linenum );
-
-// warning message for giving information about bad characters
-void bad_char_warning( const char *msg, char badchar, int linenum );
-
 // INCOMPLETE: convert ascii character to readable form
 const std::string ascii_char_to_string( int c );
