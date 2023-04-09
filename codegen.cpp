f677c39e83ebd4be39b0021e3fce294f1c2d24ae
Beau McCartney <beau@beaumccartney.xyz>
2023-04-09 14:14:19 -0600

    prologue, epilogue, and global generation

--- codegen.cpp
+++ codegen.cpp
@@ -4,60 +4,141 @@
 
 static std::string output;
 
+// label counters
+static int global_labels   = 0,
+           function_labels = 0,
+           string_labels   = 1; // S0 is reserved for included null string
+
+inline void asm_prologue()
+{
+    output +=
+R"(
+.text
+# RTS FUNCTIONS
+
+.globl main
+main:
+    jal Lmain
+    j   halt
+
+getchar:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    li $v0, 8                       # System call for read_string
+    la $a0, getcharbuf              # Pass in buffer (size = n+1)
+    li $a1, 2                       # Size = n+1 as read_string adds null
+    syscall
+
+    lb $v0, getcharbuf
+    bne $v0, $zero, ret
+
+    li $v0, -1                      # If 0, map to -1 and return
+ret:
+    lw $ra 0($sp)
+    addi $sp $sp, 4
+    jr $ra
+
+
+prints:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    # $a0 contains the address of the string to print
+    li $v0, 4                       # System call for print_string
+    syscall
+
+    lw $ra 0($sp)
+    addi $sp $sp, 4
+    jr $ra
+
+
+# No need for stack here as
+# the program will halt after this
+halt:
+    li $v0, 10
+    syscall
+    )";
+}
+
+inline void asm_epilogue()
+{
+    output +=
+R"(
+.data
+getcharbuf:
+        .space 2
+        .align 2
+S0:
+        .byte 0
+        .align 2
+)";
+}
+
+inline void emitlabel
+( std::string code )
+{
+    output += code + '\n';
+}
+
 inline void emitinstruction
 ( std::string instruction )
 {
-    std::string result = '\t' + instruction + '\n';
-    output += result;
+    output += '\t' + instruction + '\n';
 }
 
-inline std::string emitlabel
-( std::string prefix, std::string label )
+inline std::string getlabel
+( std::string prefix, int num )
 {
-    std::string result = prefix + label + ":\n";
-    output += result;
-    return result;
+    return prefix + std::to_string( num ) + ':';
 }
 
 void pass_1_pre( ASTNode & node )
 {
-    throw PruneTraversalException();
+    switch ( node.type )
+    {
+        case AST_STRING:
+        {
+            node.stringlabel = getlabel( "S", string_labels++ );
+        } break;
+        default: break;
+    }
 }
-
 
 void pass_1_post( ASTNode & node )
 {
-    puts( "foo bar" );
 }
 
-void global_vars( ASTNode & node )
+void pass_2_cb( ASTNode & node )
 {
+    if ( node.type != AST_GLOBVAR && node.type != AST_STRING ) return;
+
+    std::string label       = "LABEL UNSET";
+    std::string instruction = "INSTRUCTION UNSET";
+
     if ( node.type == AST_GLOBVAR )
     {
         std::string type = node.symbolinfo->signature;
+        instruction      = type == "string" ? ".word S0" : ".word 0" ;
 
-        emitinstruction( ".data" );
-        std::string label = emitlabel( "G", node[ 0 ].lexeme );
-
-        if ( type == "int" || type == "bool" )
-            emitinstruction( ".word 0" );
-
-        else
-        {
-            assert( type == "string" );
-
-            // TODO: emit instruction ".word " + label of string constant
-            emitinstruction( ".word TODO" );
-        }
-
-        emitinstruction( ".text" );
+        label = getlabel( "G", global_labels++ );
+        node.symbolinfo->label = label;
     }
+    else
+    {
+        label       = node.stringlabel;
+        instruction = ".asciiz \"" + node.lexeme + "\"" ;
+    }
+
+    emitlabel( label );
+    emitinstruction( instruction );
 }
 
 void gen_code( ASTNode & root )
 {
     // EMIT PROLOGUE
     // - set up stuff and go to main
+    asm_prologue();
 
     // pass 1 - code generation
     // prepost for everything else
@@ -67,7 +148,7 @@ void gen_code( ASTNode & root )
     //  - function epilogue
     prepost( root, pass_1_pre, pass_1_post );
 
-    // - flush output
+    asm_epilogue();
 
     // pass 2 - statically allocated things
     // preorder to emit global var declarations
@@ -75,7 +156,7 @@ void gen_code( ASTNode & root )
     //    - a label plus either a word or a string constant
     // - AST_FUNC ( ????? )
 
-    preorder( root, global_vars );
+    preorder( root, pass_2_cb );
 
     printf( "%s", output.data() );
 }
